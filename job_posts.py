ec0628860cfc8dd66603e1717d7eaa7ff5dda522
Drian <nkoadrian@gmail.com>
2024-10-29 17:47:30 -0300

    da filter

--- job_posts.py
+++ job_posts.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import kagglehub
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 
 # Download latest version
@@ -8,3 +10,22 @@ posts = pd.read_csv(f'{path}/gsearch_jobs.csv')
 
 posts.head()
 posts.info()
+
+da_posts = posts[posts['search_term']=='data analyst']
+da_posts.info()
+da_posts.columns.tolist()
+da_filter = da_posts[['title', 
+                      'company_name',
+                      'location','via',
+                      'posted_at',
+                      'schedule_type',
+                      'work_from_home',
+                      'salary',
+                      'date_time',
+                      'search_location',
+                      'salary_pay',
+                      'salary_hourly',
+                      'salary_yearly',
+                      'salary_standardized',
+                      'description_tokens']]
+da_filter.info()
